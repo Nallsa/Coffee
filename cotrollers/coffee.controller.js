bf9d53024c8f6872636ecd0ebcbadd9f0b8ca7e5
Aslanbek <saidtaahev@gmail.com>
2022-09-10 22:47:09 +0300

    coffee full end

--- cotrollers/coffee.controller.js
+++ cotrollers/coffee.controller.js
@@ -2,54 +2,78 @@ const Drink = require('../models/Coffee.model')
 
 module.exports.drinkController = {
   getDrink: async (req, res) => {
-    const drink = await Drink.find()
-    const drinkNew = drink.map((person) => {
-      const personEnd = `id: ${person._id}, drinkName: ${person.drinkName}, price: ${person.price}`
-      return personEnd
-    })
-    res.json(drinkNew)
+    try {
+      const drink = await Drink.find()
+      const drinkNew = drink.map((person) => {
+        const personEnd = `id: ${person._id}, drinkName: ${person.drinkName}, price: ${person.price}`
+        return personEnd
+      })
+      res.json(drinkNew)
+    } catch (e) {
+      res.json({ error: 'Ошибка при добавлении нового напитка' })
+    }
   },
   getDrinkInStock: async (req, res) => {
-    const drinkInStock = await Drink.find()
-    const drinkInStockNew = drinkInStock.filter((person) => {
-      if (person.available === true) {
-        return true
-      }
-    })
+    try {
+      const drinkInStock = await Drink.find()
+      const drinkInStockNew = drinkInStock.filter((person) => {
+        if (person.available === true) {
+          return true
+        }
+      })
 
-    const drinkInStock2 = drinkInStockNew.map((person) => {
-      const personEnd = `id: ${person._id}, drinkName: ${person.drinkName}, price: ${person.price}`
-      return personEnd
-    })
-    res.json(drinkInStock2)
+      const drinkInStock2 = drinkInStockNew.map((person) => {
+        const personEnd = `id: ${person._id}, drinkName: ${person.drinkName}, price: ${person.price}`
+        return personEnd
+      })
+      res.json(drinkInStock2)
+    } catch (e) {
+      res.json({ error: 'Ошибка при добавлении нового напитка' })
+    }
   },
   getDrinkID: async (req, res) => {
-    res.json(await Drink.findById(req.params.id))
+    try {
+      res.json(await Drink.findById(req.params.id))
+    } catch (e) {
+      res.json({ error: 'Ошибка при добавлении нового напитка' })
+    }
   },
   postDrink: async (req, res) => {
-    await Drink.create({
-      drinkName: req.body.drinkName,
-      price: req.body.price,
-      available: req.body.available,
-      caffeine: req.body.caffeine,
-      volume: req.body.volume,
-      description: req.body.description,
-    })
-    res.json('News added')
+    try {
+      await Drink.create({
+        drinkName: req.body.drinkName,
+        price: req.body.price,
+        available: req.body.available,
+        caffeine: req.body.caffeine,
+        volume: req.body.volume,
+        description: req.body.description,
+      })
+      res.json('coffee added')
+    } catch (e) {
+      res.json({ error: 'Ошибка при добавлении нового напитка' })
+    }
   },
   deleteDrink: async (req, res) => {
-    await Drink.findByIdAndRemove(req.params.id)
-    res.json('coffee delete')
+    try {
+      await Drink.findByIdAndRemove(req.params.id)
+      res.json('coffee delete')
+    } catch (e) {
+      res.json({ error: 'Ошибка при добавлении нового напитка' })
+    }
   },
   patchDrink: async (req, res) => {
-    await Drink.findByIdAndUpdate(req.params.id, {
-      drinkName: req.body.drinkName,
-      price: req.body.price,
-      available: req.body.available,
-      caffeine: req.body.caffeine,
-      volume: req.body.volume,
-      description: req.body.description,
-    })
-    res.json('coffee updated')
+    try {
+      await Drink.findByIdAndUpdate(req.params.id, {
+        drinkName: req.body.drinkName,
+        price: req.body.price,
+        available: req.body.available,
+        caffeine: req.body.caffeine,
+        volume: req.body.volume,
+        description: req.body.description,
+      })
+      res.json('coffee updated')
+    } catch (e) {
+      res.json({ error: 'Ошибка при добавлении нового напитка' })
+    }
   },
 }
